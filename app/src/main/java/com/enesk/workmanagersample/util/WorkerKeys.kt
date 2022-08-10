c9c5647929ad217e000e489915706390e36f4247
kocerenes <eneskocerr41@gmail.com>
2022-08-10 21:34:41 +0300

    Create the download worker

---
+++ app/src/main/java/com/enesk/workmanagersample/util/WorkerKeys.kt
@@ -0,0 +1,8 @@
+package com.enesk.workmanagersample.util
+
+object WorkerKeys {
+
+    const val ERROR_MSG = "errorMsg"
+    const val IMAGE_URI = "imageUri"
+    const val FILTER_URI = "filterUri"
+}
